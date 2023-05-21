d1d534961c5f16687688169484302ed52fe9530e
lojith vinsuka <lojithvinsuka1998@gmail.com>
2023-05-21 10:58:27 +0530

    seed data

--- seeder/seed.ts
+++ seeder/seed.ts
@@ -1,8 +1,32 @@
-import express from 'express'
-const app = express()
-const port = 4000
+import express from "express";
+import { questionsAndAnswers } from "./dataset";
+const mongoose = require("mongoose");
+const app = express();
+const port = 4000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
+main()
+  .then(() => seedData())
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
+
+const datasetSchema = new mongoose.Schema({
+  q: String,
+  a: String,
+});
+
+const Dataset = mongoose.model("Dataset", datasetSchema);
+
+const seedData = async () => {
+  for (const data of questionsAndAnswers) {
+    const newData = new Dataset({ q: data.q, a: data.a });
+    await newData.save();
+    console.log("saved")
+  }
+};
